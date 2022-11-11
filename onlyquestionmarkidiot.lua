e559286a477c9302158905c51402fdf9c4c570e1
thebigLisgood <116508260+thebigLisgood@users.noreply.github.com>
2022-11-11 14:33:46 +0200

    Update onlyquestionmarkidiot.lua

--- onlyquestionmarkidiot.lua
+++ onlyquestionmarkidiot.lua
@@ -1,160 +1,87 @@
 local Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
 
-if not entityTable then getgenv().entityTable={} end
-return {
-    createEntity=function(name)
-        entityTable[name]={
-            Speed=120,
-            BreakLights=true,
-            FlickerLenght=1,
-            Model=nil,
-            Height=5.2,
-            CanKill = true,
-            KillRange = 50,
-            Ambush={
-                Enabled=false,
-                MinCycles=4,
-                MaxCycles=7,
-                AmbienceMusic=workspace.Ambience_Ambush,
-                WT_Increase=0.2,
-                Speed_Increase=25
+-- Create entity
+local entity = Creator.createEntity({
+    CustomName = "???", -- Custom name of your entity
+    Model = "https://github.com/thebigLisgood/something/raw/main/none.rbxm", -- Can be GitHub file or rbxassetid
+    Speed = 120, -- Percentage, 100 = default Rush speed
+    DelayTime = 0, -- Time before starting cycles (seconds)
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 50,
+    BreakLights = true,
+    BackwardsMovement = false,
+    FlickerLights = {
+        true, -- Enabled/Disabled
+        1, -- Time (seconds)
+    },
+    Cycles = {
+        Min = 4,
+        Max = 7,
+        WaitTime = 1,
+    },
+    CamShake = {
+        true, -- Enabled/Disabled
+        {3.5, 20, 0.1, 1}, -- Shake values (don't change if you don't know)
+        100, -- Shake start distance (from Entity to you)
+    },
+    Jumpscare = {
+        false, -- Enabled/Disabled
+        {
+            Image1 = "rbxassetid://10483855823", -- Image1 url
+            Image2 = "rbxassetid://10483999903", -- Image2 url
+            Shake = true,
+            Sound1 = {
+                10483790459, -- SoundId
+                { Volume = 0.5 }, -- Sound properties
             },
-            Sounds={"PlaySounds", "Footsteps"},
-            WaitTime=0,
-            Shaking={
-                Enabled=true,
-                Config={15,8.8,0,2,1,6},
-                ActivateAtStuds=35
-            }
-        }
-    end,
-    runEntity=function(name)
-        local entityObject=entityTable[name]
-        local currentModel=entityObject.Model
-        if type(entityObject.Model)=="string" then
-            pcall(makefolder, "Entities")
-            if not isfile("Entites/"..name..".txt") then
-                writefile("Entities/"..name..".txt", game:HttpGet(entityObject.Model))
-            end
-            currentModel=game:GetObjects((getcustomasset or getsynasset)("Entities/"..name..".txt"))[1]
-        end
-        local room_l=workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]
-        local room_f=(workspace.CurrentRooms:FindFirstChildOfClass("Model").Name=="0" and workspace.CurrentRooms["1"] or workspace.CurrentRooms:FindFirstChildOfClass("Model"))
+            Sound2 = {
+                10483837590, -- SoundId
+                { Volume = 0.5 }, -- Sound properties
+            },
+            Flashing = {
+                true, -- Enabled/Disabled
+                Color3.fromRGB(255, 255, 255), -- Color
+            },
+            Tease = {
+                true, -- Enabled/Disabled
+                Min = 1,
+                Max = 3,
+            },
+        },
+    },
+    CustomDialog = {"You have died to entity called ???", "??? ???? ?? ?????? ", "??? ??? ???? ??? crucifix", "???"}, -- Custom death message
+})
 
-        currentModel.Parent=workspace
-        currentModel:FindFirstChildOfClass("Part").CanCollide=false
-        if not room_f:FindFirstChild("RoomStart") then
-            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
-                if room:FindFirstChild("RoomStart") then room_f=room; break end
-            end
-        end
-        currentModel:MoveTo(room_f:FindFirstChild("RoomStart").Position + Vector3.new(0,entityObject.Height,0))
-        require(game.ReplicatedStorage.ClientModules.Module_Events).flickerLights(tonumber(room_l.Name), entityObject.FlickerLenght)
+-----[[ Advanced ]]-----
+entity.Debug.OnEntitySpawned = function(entityTable)
+    print("Entity has spawned:", entityTable.Model)
+end
 
-        if entityObject.Ambush.Enabled then
-            local sounds={currentModel:FindFirstChild(entityObject.Sounds[1], true), currentModel:FindFirstChild(entityObject.Sounds[2], true)}
-            sounds[1]:Play(); sounds[2]:Play()
-            
-            local ogVol=sounds[1].Volume
-            task.wait()
-            sounds[1].Volume=0
-            game:GetService("TweenService"):Create(currentModel:FindFirstChild(entityObject.Sounds[1], true), TweenInfo.new(6, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), {
-                Volume=ogVol
-            }):Play()
+entity.Debug.OnEntityDespawned = function(entityTable)
+    print("Entity has despawned:", entityTable.Model)
+end
 
-            local a=entityObject.Ambush.AmbienceMusic:Clone()
-            a.Volume=0
-            a.Parent=workspace
-            a:Play()
-            delay(10, function() a:Destroy() end)
+entity.Debug.OnEntityStartMoving = function(entityTable)
+    print("Entity has started moving:", entityTable.Model)
+end
 
-            task.wait(entityObject.WaitTime)
-           
-            if entityObject.Shaking.Enabled then
-                local cameraShaker = require(game.ReplicatedStorage.CameraShaker)
-                local camera = workspace.CurrentCamera
-    
-                local camShake = cameraShaker.new(Enum.RenderPriority.Camera.Value, function(cf)
-                    camera.CFrame = camera.CFrame * cf
-                end)
-    
-                camShake:Start()
-                coroutine.create(function() 
-                    while true do
-                        if (currentModel:FindFirstChildOfClass("Part").Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= entityObject.Shaking.ActivateAtStuds then
-                            camShake:ShakeOnce(unpack(entityObject.Shaking.Config))
-                        end
-                        task.wait(.2)
-                    end
-                end)
-            end
-            
-            local rng=math.random(entityObject.Ambush.MinCycles, entityObject.Ambush.MaxCycles)
-            local cycles=0
-            repeat
-                local rooms=workspace.CurrentRooms:GetChildren()
-                for _, room in pairs(rooms) do
-                    if not room:FindFirstChild("Nodes") or tonumber(room.Name)>tonumber(room_l.Name) then continue end
-        
-                    local nodes=room.Nodes:GetChildren()
-                    local entityPart=currentModel:FindFirstChildOfClass("Part")
-                    for _, node in pairs(nodes) do
-                        local timeC = (math.abs((node.Position - entityPart.Position).Magnitude)) / entityObject.Speed
-                        game:GetService("TweenService"):Create(entityPart, TweenInfo.new(timeC, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), {
-                            CFrame = CFrame.new(node.CFrame.X, node.CFrame.Y + entityObject.Height, node.CFrame.Z),
-                        }):Play()
-                        if entityObject.BreakLights then require(game.ReplicatedStorage.ClientModules.Module_Events).breakLights(room) end
-                        task.wait(timeC)
-                    end
-                end
-                for i=#rooms, 1, -1 do
-                    local room=rooms[i]
-                    if not room:FindFirstChild("Nodes") then continue end
-        
-                    local nodes=room.Nodes:GetChildren()
-                    local entityPart=currentModel:FindFirstChildOfClass("Part")
-                    for k=#nodes, 1, -1 do
-                        local node=nodes[k]
-                        local timeC = (math.abs((node.Position - entityPart.Position).Magnitude)) / entityObject.Speed
-                        game:GetService("TweenService"):Create(entityPart, TweenInfo.new(timeC, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), {
-                            CFrame = CFrame.new(node.CFrame.X, node.CFrame.Y + entityObject.Height, node.CFrame.Z),
-                        }):Play()
-                        if entityObject.BreakLights then require(game.ReplicatedStorage.ClientModules.Module_Events).breakLights(room) end
-                        task.wait(timeC)
-                    end
-                end
-                cycles+=1
-                entityObject.Speed+=entityObject.Ambush.Speed_Increase
-                entityObject.WaitTime+=entityObject.Ambush.WT_Increase
-                task.wait(entityObject.WaitTime)
-            until cycles==rng
-            task.wait(.5)
-            currentModel:FindFirstChildOfClass("Part").Anchored=false; currentModel:FindFirstChildOfClass("Part").CanCollide=false;
-            room_l:WaitForChild("Door").ClientOpen:FireServer()
-            
-        else
-            currentModel:FindFirstChild(entityObject.Sounds[1], true):Play(); currentModel:FindFirstChild(entityObject.Sounds[2], true):Play()
-            task.wait(entityObject.WaitTime)
-    
-            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
-                if not room:FindFirstChild("Nodes") then continue end
-    
-                local nodes=room.Nodes:GetChildren()
-                local entityPart=currentModel:FindFirstChildOfClass("Part")
-                for _, node in pairs(nodes) do
-                    local timeC = (math.abs((node.Position - entityPart.Position).Magnitude)) / entityObject.Speed
-                    game:GetService("TweenService"):Create(entityPart, TweenInfo.new(timeC, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), {
-                        CFrame = CFrame.new(node.CFrame.X, node.CFrame.Y + entityObject.Height, node.CFrame.Z),
-                    }):Play()
-                    if entityObject.BreakLights then require(game.ReplicatedStorage.ClientModules.Module_Events).breakLights(room) end
-                    task.wait(timeC)
-                end
-                if room == room_l then
-                    task.wait(.5)
-                    entityPart.Anchored=false; entityPart.CanCollide=false;
-                    room_l:WaitForChild("Door").ClientOpen:FireServer()
-                end
-            end
-        end
-    end
-}
+entity.Debug.OnEntityFinishedRebound = function(entityTable)
+    print("Entity has finished rebound:", entityTable.Model)
+end
+
+entity.Debug.OnEntityEnteredRoom = function(entityTable, room)
+    print("Entity:", entityTable.Model, "has entered room:", room)
+end
+
+entity.Debug.OnLookAtEntity = function(entityTable)
+    print("Player has looked at entity:", entityTable.Model)
+end
+
+entity.Debug.OnDeath = function(entityTable)
+    warn("Player has died.")
+end
+------------------------
+
+-- Run the created entity
+Creator.runEntity(entity)
