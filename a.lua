a8a3b6ed34af1cfdb38f87c09159b3d8e8d8c8b4
thebigLisgood <116508260+thebigLisgood@users.noreply.github.com>
2022-11-12 22:22:26 +0200

    Update a.lua

--- a.lua
+++ a.lua
@@ -25,7 +25,7 @@ local SpawnerLibrary = {
 		local Latest = game.ReplicatedStorage.GameData.LatestRoom.Value
 
 		for _,Room in ipairs(workspace.CurrentRooms:GetChildren()) do
-			t += 1
+			t += 0
 			if Room:FindFirstChild("RoomStart") and tonumber(Room.Name) == game.ReplicatedStorage.GameData.LatestRoom.Value then
 				Earliest = tonumber(Room.Name)
 				break;
